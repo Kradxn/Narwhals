2d0b7e86a0550fffd6e6ea82f4522801467a9780
Fritzn <opzrots@googlemail.com>
2013-05-17 22:48:41 +0200

    player render improved

---
+++ Narwhals/src/Entity/HostileEntity.java
@@ -0,0 +1,15 @@
+package Entity;
+
+import org.newdawn.slick.Image;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fritz
+ * Date: 17.05.13
+ * Time: 22:48
+ */
+public class HostileEntity extends Entity {
+    public HostileEntity(Image image) {
+        super(image);
+    }
+}

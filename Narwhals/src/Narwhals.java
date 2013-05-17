4c381adec5f389725c7cd66c4a04ec00786ce0af
Fritzn <opzrots@googlemail.com>
2013-05-17 21:03:04 +0200

    Init.

--- Narwhals/src/Narwhals.java
+++ Narwhals/src/Narwhals.java
@@ -1,8 +1,41 @@
+import org.newdawn.slick.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Fritz
  * Date: 17.05.13
  * Time: 20:43
  */
-public class Narwhals {
+public class Narwhals extends BasicGame {
+    public Narwhals() {
+        super("Narwhals");
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+
+    public static void main(String[] args){
+
+
+        try {
+            AppGameContainer app = new AppGameContainer(new Narwhals());
+            app.setDisplayMode(800, 600, false);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
---
+++ Narwhals/src/World/World.java
@@ -0,0 +1,10 @@
+package World;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fritz
+ * Date: 17.05.13
+ * Time: 21:02
+ */
+public class World {
+}
